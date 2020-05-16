9c29c699b213a8822a5ed3d6ee15116b145e629a
Eddie-Z <35039865+Eddie-Z@users.noreply.github.com>
2020-05-16 15:14:57 -0700

    middleware and routes

--- app.js
+++ app.js
@@ -4,8 +4,23 @@ const express = require('express');
 //execute express
 const app = express();
 
-//create route and listen to server
+//middleware in express functinos that execute on routes
+//authentications are used here
+app.use('/posts',() => {
+    console.log("Middleware") //callback function
+});
+
+//create route
+//parameters:
+// / is route
 app.get('/', (req,res) =>{
+    //respond to user
     res.send("we are at home");
 });
+app.get('/posts', (req,res) =>{
+    //respond to user
+    res.send("post route");
+});
+
+//listen on port
 app.listen(3000);
